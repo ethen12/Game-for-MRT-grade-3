15cf4cf0ff1aee7185c090752e8b23ee91f12444
ethen12 <47332656+ethen12@users.noreply.github.com>
2019-04-29 14:01:58 -0400

    Update sktech.js

--- sktech.js
+++ sktech.js
@@ -16,7 +16,8 @@ Button.prototype.draw = function() {
   fill(255,255,255);
   rect(this.x, this.y, this.width, this.height, 5);
   fill(0,0,0);
-  textSize(19);
+  textSize(10);
+  textAlign(LEFT, TOP);
   text(this.label, this.x+10, this.y+this.height/4);
 };
 
@@ -24,18 +25,30 @@ Button.prototype.isMouseInside = function(){
   return mouseX > this.x &&
          mouseX < (this.x + this.width) &&
          mouseY > this.y &&
-         mouseY < (this.y + thisheight);
+         mouseY < (this.y + this.height);
 };
 
 var btn1 = new Button({
       x: 80,
       y: 280,
-      label: "START"
+      label: "START",
 });
 
-btn1.draw();
+var btn2 = new Button({
+      x: 280,
+      y: 280,
+      label: "RULES",
+});
 
-
+var btn3 = new Button({
+      x: 180,
+      y: 350,
+      label: "ABOUT US",
+});
+mouseClicked = function() {
+  if (btn1.isMouseInside()) {
+    sceneNum++
+  }
 
 function draw() {
   if(sceneNum===0){
@@ -55,18 +68,14 @@ function draw() {
   text("EQUATION EXPEDITION",60,220);
   noFill();
   stroke(255,255,255);
-  rect(53,160,300,100);//title outline
+  rect(53,165,300,100);//title outline
   stroke(0,0,0);
   fill(255,255,255);
   textSize(13);
   text("Developed By TEAM GAME",180,250);
-  fill(255,255,255);
-  fill(0,0,0);
-  text("START",90,300);
-  text("RULES",290,300);
-  fill(0,0,0);
-  textSize(11);
-  text("ABOUT US",182,370);
+  btn1.draw();
+  btn2.draw();
+  btn3.draw();
   }else if(sceneNum===1){
     
   }
